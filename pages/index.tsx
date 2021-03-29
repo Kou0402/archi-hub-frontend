2b498045cbe395ac3dd28692281520f9b8e475b5
kou0402 <kou.4869.biz@gmail.com>
2021-03-29 20:24:07 +0900

    chore: TypeScript を導入する

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { NextPage } from 'next'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <main>Hello Next!!</main>
+    </div>
+  );
+}
+
+export default Home
