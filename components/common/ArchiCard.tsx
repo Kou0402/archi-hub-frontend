f7ce4a597fbaf28e571945b6f8fdf6db0f990434
kou0402 <kou.4869.biz@gmail.com>
2021-04-17 14:16:54 +0900

    feat: アーキテクチャを表示するカードコンポーネントを追加する

---
+++ components/common/ArchiCard.tsx
@@ -0,0 +1,41 @@
+import React from 'react'
+
+export type ArchiCardProps = {
+  appTitle: string
+  appType: string
+  appScale: string
+  appElements: Array<string>
+}
+
+export const ArchiCard: React.FC<ArchiCardProps> = (props) => {
+  return (
+    <article className="flex items-center pt-2 pb-2 pl-5 pr-5">
+      <a>
+        <img src="/web-icon.svg"></img>
+      </a>
+      <div className="ml-5">
+        <h1 className="text-2xl">{props.appTitle}</h1>
+        <ul className="flex text-sm">
+          <li>{props.appType}</li>
+          <li className="ml-3">{props.appScale}</li>
+        </ul>
+        <ul className="flex text-sm">
+          {props.appElements.map((appElement, i) => {
+            return (
+              <li className={i == 0 ? '' : 'ml-3'} key={appElement}>
+                {appElement}
+              </li>
+            )
+          })}
+        </ul>
+        <ul className="flex items-end text-sm">
+          <li>
+            <img src="/archi-hub-logo.svg" width="30px"></img>
+          </li>
+          <li className="ml-1">useName</li>
+          <li className="ml-1"> createdAt</li>
+        </ul>
+      </div>
+    </article>
+  )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,22 @@
 import React from 'react'
 import { NextPage } from 'next'
 import { Hero } from 'components/page/top/Hero'
+import { ArchiCard, ArchiCardProps } from 'components/common/ArchiCard'
 
 const Home: NextPage = () => {
+  const archiCardProps: ArchiCardProps = {
+    appTitle: 'アプリタイトル',
+    appType: 'Webアプリ',
+    appScale: '中規模',
+    appElements: ['React', 'Node', 'AWS', 'DynamoDB'],
+  }
   return (
     <main className="text-lg">
       <Hero></Hero>
+      <section className="bg-light">
+        <ArchiCard {...archiCardProps}></ArchiCard>
+        <ArchiCard {...archiCardProps}></ArchiCard>
+      </section>
     </main>
   )
 }
